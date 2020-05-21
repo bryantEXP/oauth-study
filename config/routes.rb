cee14966349e130b12fa3e7a94ff54006e9170bf
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 11:07:37 -0500

    step 18

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+# <---- LESSON ---->
+
   root to: 'application#welcome'
+  # Step Eighteen:
+  # Override controller source with the controller you just wrote
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
