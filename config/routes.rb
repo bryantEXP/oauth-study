47e3e05a32500fcf940cacff23f3affbf275fae8
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 11:08:45 -0500

    Last step

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'application#welcome'
   # Step Eighteen:
   # Override controller source with the controller you just wrote
+  # Congrats! This is the last step! Test and enjoy
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
