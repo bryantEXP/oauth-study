14068e45282110f7cfdb0ace022a9c5c04cc56c2
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 10:56:04 -0500

    step 12

--- db/migrate/20200520172331_update_users.rb
+++ db/migrate/20200520172331_update_users.rb
@@ -1,3 +1,7 @@
+# <---- LESSON ---->
+# Step Twelve:
+# add these columns to the user's table
+
 class UpdateUsers < ActiveRecord::Migration[6.0]
   def change
     add_column(:users, :provider, :string, limit: 50, null: false, default: '')
