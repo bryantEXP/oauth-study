9cc489504a7266d9fce370969fff355fb6c38f26
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 10:52:54 -0500

    steps 9 and 10

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,12 @@
+# <---- LESSON ---->
+# Step Nine:
+# Set up Active record to use devise-omniauth
+
 Rails.application.config.session_store :active_record_store, key: '_devise-omniauth_session'
+
+# <--- LESSON --->
+# Next steps are here because you haven't created the files yet.
+# Step Ten:
+# Create two migrations in this order:
+  # rails g migration update_users
+  # rails g migration create_sessions
