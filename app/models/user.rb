adeee0d36f19093c7ee5ae98c348601d389947d7
Bryant Richards <dragonseyeimages@gmail.com>
2020-05-21 10:58:45 -0500

    step 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
+         # <---- LESSON ---->
+         # Step Fourteen:
+         # Add omniauthable and pass in which ever providers you are using here.
          :omniauthable, omniauth_providers:[:facebook, :github, :google_oauth2]
 
   def self.create_from_provider_data(provider_data)
